b55d938328ac3edee2a4ed5aa0298730462ca9fc
suragch <suragch@suragch.dev>
2022-04-26 10:22:30 +0800

    added audio progress bar

--- lib/music_player_screen/music_player_screen.dart
+++ lib/music_player_screen/music_player_screen.dart
@@ -1,3 +1,4 @@
+import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
 import 'package:flutter/material.dart';
 
 class MusicPlayerScreen extends StatelessWidget {
@@ -7,12 +8,30 @@ class MusicPlayerScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Music Player')),
-      body: const Image(
-        image: AssetImage('assets/music.jpg'),
-        fit: BoxFit.cover,
-        height: double.infinity,
-        width: double.infinity,
-        alignment: Alignment.center,
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          const Image(
+            image: AssetImage('assets/music.jpg'),
+            fit: BoxFit.cover,
+            height: double.infinity,
+            width: double.infinity,
+            alignment: Alignment.center,
+          ),
+          ColoredBox(
+            color: Colors.white.withOpacity(0.7),
+          ),
+          const Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: EdgeInsets.all(20.0),
+              child: ProgressBar(
+                progress: Duration(minutes: 1),
+                total: Duration(minutes: 3, seconds: 30),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
